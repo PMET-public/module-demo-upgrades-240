4a9c6c70167094fa43a7479b0772d60ddf5c927d
Jeff Britts <jbritts@adobe.com>
2020-10-28 15:53:01 -0400

    msi data install

---
+++ Setup/Patch/Data/MsiDataInstall.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace MagentoEse\DemoUpgrades\Setup\Patch\Data;
+
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+use MagentoEse\DataInstall\Model\Process;
+use Magento\Framework\Module\Manager;
+
+class MsiDataInstall implements DataPatchInterface
+{
+    /** @var Process  */
+    protected $process;
+
+    /** @var Manager  */
+    protected $moduleManager;
+
+    public function __construct(Process $process, Manager $moduleManager)
+    {
+        $this->process = $process;
+        $this->moduleManager = $moduleManager;
+    }
+
+    public function apply()
+    {
+        /**
+         * $this->process->loadFiles(<name of module>,<data files directory (defaults to fixtures)>
+         *
+         * the data files directory can be any directory in the root of the module, or a subdirectory (fixtures/grocery)
+         */
+
+       /**Only run if this is the demo instance */
+       if($this->moduleManager->isEnabled('MagentoEse_DemoAdminConfigurations')){
+        $this->process->loadFiles('MagentoEse_DemoUpgrades','fixtures/msi_inventory/luma');
+       }
+       /** Only run if venia catalog is installed */
+       if($this->moduleManager->isEnabled('MagentoEse_VeniaCatalogSampleData')){
+        $this->process->loadFiles('MagentoEse_DemoUpgrades','fixtures/msi_inventory/venia');
+       }
+      
+    }
+
+    public static function getDependencies()
+    {
+        return [];
+    }
+
+    public function getAliases()
+    {
+        return [];
+    }
+}
